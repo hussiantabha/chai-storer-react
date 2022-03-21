9d049db78d734f2f3e1d48a35f75b7137bae3d16
hussiantabha <tabhahusain@gmail.com>
2022-03-21 21:26:37 +0530

    Update: added the product data

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
 const FilterContext = createContext({});
